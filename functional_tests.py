fa030c80a9577149abc676551d7649c906c038c3
Zhuangda Zhu <zhuangdaz@gmail.com>
2017-03-03 00:39:41 -0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -23,8 +23,7 @@ class NewVistorTest(unittest.TestCase):
 		# She is invited to enter a to-do item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
-			inputbox.get_attribute('placeholder'),
-			'Enter a to-do item'
+			inputbox.get_attribute('placeholder'), 'Enter a to-do item'
 		)
 
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
@@ -39,7 +38,8 @@ class NewVistorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table"
 		)
 
 
