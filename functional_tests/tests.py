605b0d31536925c9464b87b895f8fb27db10988d
Zhuangda Zhu <zhuangdaz@gmail.com>
2017-03-20 23:21:19 -0700

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,10 +3,21 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+import sys
 
 MAX_WAIT = 10
 
 class NewVistorTest(StaticLiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
@@ -30,7 +41,7 @@ class NewVistorTest(StaticLiveServerTestCase):
 	def test_can_start_a_list_for_one_user(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
@@ -70,7 +81,7 @@ class NewVistorTest(StaticLiveServerTestCase):
 
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 		# Edith start a new todo list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
@@ -88,7 +99,7 @@ class NewVistorTest(StaticLiveServerTestCase):
 		self.browser = webdriver.Firefox()
 
 		# Francis visits the home page. There is no sign of Edith's list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -114,7 +125,7 @@ class NewVistorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		# Edith goes t o the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		# She notices the input box is nicely centered
