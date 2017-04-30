2387c45c86ab3b72079f36a91a8f812a9310f9e7
Zhuangda Zhu <zhuangdaz@gmail.com>
2017-04-30 00:03:24 -0700

    enforce model validation in list view

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -36,6 +36,49 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % correct_list.id)
         self.assertEqual(response.context['list'], correct_list)
 
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/',
+            data={'item_text': 'A new item added to correct list'}
+        )
+
+        self.assertTrue(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item added to correct list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_POST_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/',
+            data={'item_text': 'A new item added to correct list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % correct_list.id)
+
+    def test_validation_errors_end_up_on_list_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            f'/lists/{list_.id}/',
+            data={'item_text': ''}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        list_ = List.objects.create()
+        self.client.post(f'/lists/{list_.id}/', data={'item_text': ''})
+        self.assertEqual(Item.objects.count(), 0)
+
+
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
@@ -59,32 +102,6 @@ class NewListTest(TestCase):
         self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(Item.objects.count(), 0)
         self.assertEqual(List.objects.count(), 0)
-
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            '/lists/%d/' % (correct_list.id),
-            data={'item_text': 'A new item added to correct list'}
-        )
-
-        self.assertTrue(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new item added to correct list')
-        self.assertEqual(new_item.list, correct_list)
-
-    def test_POST_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            '/lists/%d/' % correct_list.id,
-            data={'item_text': 'A new item added to correct list'}
-        )
-
-        self.assertRedirects(response, '/lists/%d/' % correct_list.id)
-
 
 
         
--- lists/views.py
+++ lists/views.py
@@ -7,18 +7,23 @@ def home_page(request):
 
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
-	item_text = request.POST.get('item_text', None)
+	error = None
 	if request.method == 'POST':
-		item = Item.objects.create(text=item_text, list=list_)
-		return redirect('/lists/%d/' % (list_.id,))
-	return render(request, 'list.html', {'list': list_})
+		try:
+			item = Item.objects.create(text=request.POST.get('item_text', None), list=list_)
+			item.full_clean()
+			return redirect(f'/lists/{list_.id}/')
+		except ValidationError:
+			item.delete()
+			error = "You can't have an empty list item"
+		
+	return render(request, 'list.html', {'list': list_, 'error': error})
 
 def new_list(request):
 	list_ = List.objects.create()
 	item = Item.objects.create(text=request.POST['item_text'], list=list_)
 	try:
 		item.full_clean()
-		item.save()
 	except ValidationError:
 		list_.delete()
 		error = "You can't have an empty list item"
