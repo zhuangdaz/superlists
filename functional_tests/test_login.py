367f6c65aeb522be2b20cbdd339cd0cd0230947f
Zhuangda Zhu <zhuangdaz@gmail.com>
2017-06-07 00:19:12 -0700

    Add logout logic

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -45,3 +45,15 @@ class LoginTest(FunctionalTest):
         )
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(TEST_EMAIL, navbar.text)
+
+        # she is logging out
+        self.browser.find_element_by_link_text('Log out').click()
+
+        # She is logged out
+        self.wait_for(
+            lambda: self.browser.find_element_by_name('email')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(TEST_EMAIL, navbar.text)
+
+
