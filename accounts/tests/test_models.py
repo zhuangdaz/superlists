3f0ed6ccd7a3d9592cb44fed6dddd76d596e2d47
Zhuangda Zhu <zhuangdaz@gmail.com>
2017-06-06 22:58:18 -0700

    Custom passwordless auth backend + custom user model

---
+++ accounts/tests/test_models.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from django.contrib import auth
+from accounts.models import Token
+
+
+User = auth.get_user_model()
+
+class UserModelTest(TestCase):
+    def test_user_is_valid_with_email_only(self):
+        user = User(email='a@b.com')
+        user.full_clean() # should not raise
+
+    def test_email_is_primary_key(self):
+        user = User()
+        self.assertFalse(hasattr(user, 'id'))
+
+    def test_no_problem_with_auth_login(self):
+        user = User.objects.create(email='edith@example.com')
+        user.backend = ''
+        request = self.client.request().wsgi_request
+        auth.login(request, user)
+
+class TokenModelTest(TestCase):
+    def test_links_user_with_auto_generated_uid(self):
+        token1 = Token.objects.create(email='a@b.com')
+        token2 = Token.objects.create(email='a@b.com')
+
+        self.assertNotEqual(token1.uid, token2.uid)
+
+
--- superlists/settings.py
+++ superlists/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
+]
+
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +143,9 @@ LOGGING = {
     },
     'root': {'level': 'INFO'},
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'noreply.mainecoon@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_PORT=587
