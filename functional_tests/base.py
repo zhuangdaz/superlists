a42735179cc80433f1a074a97f7e440177927ca7
Zhuangda Zhu <zhuangdaz@gmail.com>
2017-06-11 11:54:46 -0700

    test_my_lists: precreate sessions, move login checks into base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -22,6 +22,20 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def wait_to_be_logged_in(self, email):
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Log out')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+    def wait_to_be_logged_out(self, email):
+        self.wait_for(
+            lambda: self.browser.find_element_by_name('email')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
